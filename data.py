db1d07c8081cf9e17f3d75f3187a1ea8dc3e6f52
Kate Pyankova <pyankate@gmail.com>
2024-06-04 20:50:15 +0300

    add allure report

--- data.py
+++ data.py
@@ -42,3 +42,9 @@ class CreateOrderData:
         "BLACK"
     ]
     }
+
+class Urls:
+    BASE_URL = 'http://qa-scooter.praktikum-services.ru'
+    CREATE_COURIER = 'http://qa-scooter.praktikum-services.ru/api/v1/courier'
+    LOGIN_COURIER = 'http://qa-scooter.praktikum-services.ru/api/v1/courier/login'
+    ORDER = 'http://qa-scooter.praktikum-services.ru/api/v1/orders'
--- urls.py
+++
@@ -1,4 +0,0 @@
-BASE_URL = 'http://qa-scooter.praktikum-services.ru'
-CREATE_COURIER = 'http://qa-scooter.praktikum-services.ru/api/v1/courier'
-LOGIN_COURIER = 'http://qa-scooter.praktikum-services.ru/api/v1/courier/login'
-ORDER = 'http://qa-scooter.praktikum-services.ru/api/v1/orders'
